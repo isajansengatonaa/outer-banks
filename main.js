a1fa4989f06ec075d321ea0382597e87a02b263e
isajansengatonaa <163419308+isajansengatonaa@users.noreply.github.com>
2024-03-15 10:06:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://mixdeseries.com.br/wp-content/uploads/2023/06/Outer-Banks.webp";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://portalpopline.com.br/wp-content/uploads/2024/02/outer-banks-capa-758x570.webp";
+}
